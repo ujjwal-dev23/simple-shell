8c3ed4d0e81d148cebf1ba6c81467ef84608c3e0
Ujjwal <ujjwalkandera23@tutamail.com>
2026-07-15 15:39:35 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  while (true) {
+    std::cout << "$ ";
 
-  std::string command;
-  std::getline(std::cin, command);
+    std::string command;
+    std::getline(std::cin, command);
 
-  std::cout << command << ": command not found\n";
+    std::cout << command << ": command not found\n";
+  }
 }
