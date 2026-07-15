27f605e0db169ae8b021fe0d2b18ffbcd5894550
Ujjwal <ujjwalkandera23@tutamail.com>
2026-07-15 19:23:13 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+std::vector<std::string> split_string_into_args(std::string original_string) {
+    std::stringstream ss(original_string);
+    std::string word;
+    std::vector<std::string> words;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    return words;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -14,9 +28,18 @@ int main() {
     std::string command;
     std::getline(std::cin, command);
 
+    std::vector<std::string> args(split_string_into_args(command));
+
     // Currently case-sensitive
-    if (command == "exit") {
+    if (args[0] == "exit") {
         running = false;
+    }
+    else if (args[0] == "echo") {
+        for (int i = 1; i < args.size(); i++) {
+            std::cout << args[i] << ' ';
+        }
+
+        std::cout << '\n';
     }
     else {
         std::cout << command << ": command not found\n";
